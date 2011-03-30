2bb19ef1e50c69ba1d9266092f970e9562f02b9a
Brian Jordan <bcjordan@gmail.com>
2011-03-30 18:07:11 -0400

    getting close, now bankers' rounding

--- 2/solution.rb
+++ 2/solution.rb
@@ -4,10 +4,18 @@
 
 require 'rexml/document'
 require 'pp' # TODO REMOVE
+require 'csv'
 
-rate_table = {}
+target_item = 'DM1182'
+target_currency = 'USD'
+rate_file = 'SAMPLE_RATES.xml'
+transaction_file = 'SAMPLE_TRANS.csv'
 
-rates_file = File.read('RATES.xml')
+@rate_table = {}
+costs_list = []
+total = 0.0
+
+rates_file = File.read(rate_file)
 
 xml_doc = REXML::Document.new(rates_file)
 
@@ -16,9 +24,44 @@ xml_doc.elements.each('rates/rate') do |rate_elt|
   from, to   = rate_data['from'].text, rate_data['to'].text
   conversion = rate_data['conversion'].text.to_f
 
-  rate_table[[from, to]] = conversion
+  @rate_table[[from, to]] = conversion
 end
 
-pp rate_table
+CSV.read(transaction_file)[1..-1].each do |row|
+  costs_list << row[2] if row[1] == target_item
+end
+
+def get_rate (currency, target_currency, rate)
+  if currency == target_currency
+    rate
+  elsif @rate_table[[currency, target_currency]] != nil
+    @rate_table[[currency, target_currency]]
+  else
+    @rate_table.each do |k, v|
+      if k[0] == currency
+         rate = get_rate(k[1], target_currency, rate * v)
+         return rate if rate
+      end
+    end
+    return nil 
+  end
+end
+
+def truncator n
+  pp (n * 100).round.to_f / 100
+  (n * 100).round.to_f / 100
+end
+
+costs_list.each do |cost|
+  number, currency = cost.split[0].to_f, cost.split[1]
+  if currency == target_currency
+    total += number
+    pp number
+  else
+    total += truncator(number * get_rate(currency, target_currency, 1))
+  end
+end
+
+pp total
 
 
