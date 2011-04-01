8462367fde17a2260ace1f7ace94083d4aa4a4d9
Brian Jordan <bcjordan@gmail.com>
2011-04-01 10:34:35 -0400

    solved 2

--- 2/solution.rb
+++ 2/solution.rb
@@ -1,15 +1,11 @@
-# import RATES.xml into lookup table { [from, to] => rate }
-# import TRANS.csv
-#
-
 require 'rexml/document'
-require 'pp' # TODO REMOVE
 require 'csv'
 
 target_item = 'DM1182'
 target_currency = 'USD'
-rate_file = 'SAMPLE_RATES.xml'
-transaction_file = 'SAMPLE_TRANS.csv'
+rate_file = 'RATES.xml'
+transaction_file = 'TRANS.csv'
+solution_file = 'OUTPUT.txt'
 
 @rate_table = {}
 costs_list = []
@@ -31,11 +27,15 @@ CSV.read(transaction_file)[1..-1].each do |row|
   costs_list << row[2] if row[1] == target_item
 end
 
+#
+# Perform breadth-first search for rate from currency to target currency,
+# given an initial rate. Uses rate as an accumulating parameter.
+#
 def get_rate (currency, target_currency, rate)
   if currency == target_currency
     rate
   elsif @rate_table[[currency, target_currency]] != nil
-    @rate_table[[currency, target_currency]]
+    @rate_table[[currency, target_currency]] * rate
   else
     @rate_table.each do |k, v|
       if k[0] == currency
@@ -43,25 +43,34 @@ def get_rate (currency, target_currency, rate)
          return rate if rate
       end
     end
-    return nil 
+    nil 
   end
 end
 
-def truncator n
-  pp (n * 100).round.to_f / 100
-  (n * 100).round.to_f / 100
+def bankers_rounder n
+  cents                = n.to_s.split('.')[1]
+  thousandth, hundreth = cents[2..2].to_i, cents[1..1].to_i
+
+  if    thousandth < 5
+    (n * 100).truncate / 100.0
+  elsif thousandth > 5
+    ((n * 100).truncate + 1) / 100.0
+  else
+    if hundreth.odd?
+      ((n * 100).truncate + 1) / 100.0
+    elsif hundreth.even? 
+      ((n * 100).truncate + 1) / 100.0
+    end
+  end
 end
 
 costs_list.each do |cost|
   number, currency = cost.split[0].to_f, cost.split[1]
   if currency == target_currency
     total += number
-    pp number
   else
-    total += truncator(number * get_rate(currency, target_currency, 1))
+    total += bankers_rounder(number * get_rate(currency, target_currency, 1))
   end
 end
 
-pp total
-
-
+open(solution_file, 'w') { |f| f << "#{total}\n" }
